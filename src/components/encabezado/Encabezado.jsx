56c455bea1c611a68cb1796b9dbbcf4bb44ce356
Juan José Hidalgo Neira <tempocompartido@hotmail.com>
2023-02-13 18:03:04 +0100

    2º commit

---
+++ src/components/encabezado/Encabezado.jsx
@@ -0,0 +1,11 @@
+
+function Encabezado() {
+    return (
+      <>
+        <h1 className="Encabezado">Encabezado</h1>
+        <h2>Encabezado 2</h2>
+      </>
+    );
+  }
+  
+  export default Encabezado;
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,12 @@
+
+function Login() {
+  return (
+    <form className="login">
+        <input type="test" name="usuario"/>
+        <input type="password" name="contrasinal"/>
+        <input type="submit" value="Enviar"/>
+    </form>
+  );
+}
+
+export default Login;
