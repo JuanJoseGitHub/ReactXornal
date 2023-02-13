fb0586374fc1684bd939b87db67fe3be0655bf7b
Juan José Hidalgo Neira <tempocompartido@hotmail.com>
2023-02-13 19:47:38 +0100

    reubicar imaxes

--- src/components/encabezado/Encabezado.jsx
+++ src/components/encabezado/Encabezado.jsx
@@ -1,9 +1,9 @@
-import logo from "./phone.jpg"
+import telefono from "../../images/phone.jpg"
 function Encabezado() {
     return (
       <>
         <h1 className="Encabezado">Encabezado</h1>
-        <img src={logo} alt="phone" width="150" height="150" />
+        <img src={telefono} alt="phone" width="150" height="150" />
         <h2>Encabezado 2</h2>
       </>
     );
