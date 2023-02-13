25b175377a68b2fd2ad3e772b4dbc8414c300264
Juan José Hidalgo Neira <tempocompartido@hotmail.com>
2023-02-13 19:26:23 +0100

    props

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import './components/login/Login'
 import Login from './components/login/Login';
 import Encabezado from './components/encabezado/Encabezado';
+import Nova from './components/nova/Nova';
 
 function App() {
   return (
     <>
-      <h1>Test</h1>
       <Login></Login>
       <Encabezado></Encabezado>
-
+      <Nova titulo="Este é o título por props" contido="contido nº 1"></Nova>
+      <Nova titulo="Este é o outro título" contido="contido nº 2"></Nova>
     </>
   );
 }
---
+++ src/components/nova/Nova.jsx
@@ -0,0 +1,10 @@
+function Nova(props) {
+    return (
+      <>
+      <h2>{props.titulo}</h2>
+      <p>{props.contido}</p>
+      </>
+       );
+  }
+  
+  export default Nova;
